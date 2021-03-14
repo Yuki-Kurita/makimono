e8da80997eaeb96e42a0e66befed2e6b521529f4
Yuki-Kurita <258qu1to8yodeg1@gmail.com>
2021-03-14 20:13:36 +0900

    tailwindの導入:art:

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,12 +5,18 @@ import React from "react";
 const LandingPage: React.FC = () => {
   return (
     <Layout>
-      <div>header</div>
       <main>
         LP説明いろいろ
         <Link href="/explore">
           <a>Get Start</a>
         </Link>
+        <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+          <div className="flex-shrink-0"></div>
+          <div>
+            <div className="text-xl font-medium text-black">ChitChat</div>
+            <p className="text-gray-500">You have a new message!</p>
+          </div>
+        </div>
       </main>
     </Layout>
   );
