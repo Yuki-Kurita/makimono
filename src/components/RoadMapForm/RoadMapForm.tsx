eec79cf6d4ac26cea3f4e30e39aaa9f72d4c99e1
Yuki-Kurita <258qu1to8yodeg1@gmail.com>
2021-04-11 17:39:50 +0900

    roadmapをreduxで管理:sparkles:

--- src/components/RoadMapForm/RoadMapForm.tsx
+++ src/components/RoadMapForm/RoadMapForm.tsx
@@ -1,27 +1,63 @@
-import React from "react";
+import { useAppDispatch } from "@/store/hooks";
+import { setRoadmap } from "@/store/roadmap/roadmapSlice";
+import React, { useState } from "react";
 
-interface RoadMapFormProps {}
+interface RoadMapFormProps {
+  id: number;
+}
 
-const RoadMapForm: React.FC<RoadMapFormProps> = () => {
+interface FormProps {
+  id: number;
+  title: string;
+  url: string;
+  explain: string;
+}
+
+const RoadMapForm: React.FC<RoadMapFormProps> = ({ id }) => {
+  const dispatch = useAppDispatch();
+  const [formValue, setFormValue] = useState<FormProps>({
+    id,
+    title: "",
+    url: "",
+    explain: "",
+  });
   return (
-    <div className="grid grid-cols-2 py-4 px-2 rounded-lg bg-white">
+    <div className="grid grid-cols-2 mt-4 py-4 px-2 rounded-lg bg-white">
       <div className="col-span-2 mb-4">
-        <div className="no ml-3">1</div>
+        <div className="no ml-3">{id}</div>
       </div>
       <input
         className="appearance-none bg-transparent border-b border-teal-500 w-4/5 text-gray-700 mx-auto py-1 px-2 leading-tight focus:outline-none"
         type="text"
         placeholder="Title"
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          setFormValue((formValue) => {
+            return { ...formValue, title: e.target.value };
+          })
+        }
+        onBlur={() => dispatch(setRoadmap(formValue))}
       />
       <input
         className="appearance-none bg-transparent border-b border-teal-500 w-4/5 text-gray-700 mx-auto py-1 px-2 leading-tight focus:outline-none"
         type="url"
         placeholder="ロードマップのURL"
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          setFormValue((formValue) => {
+            return { ...formValue, url: e.target.value };
+          })
+        }
+        onBlur={() => dispatch(setRoadmap(formValue))}
       />
       <input
         className="col-span-2 appearance-none bg-transparent border-b border-teal-500 w-11/12 text-gray-700 mx-auto mt-5 py-1 px-2 leading-tight focus:outline-none"
         type="text"
         placeholder="説明"
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          setFormValue((formValue) => {
+            return { ...formValue, explain: e.target.value };
+          })
+        }
+        onBlur={() => dispatch(setRoadmap(formValue))}
       />
     </div>
   );
--- src/pages/roadmaps/new.tsx
+++ src/pages/roadmaps/new.tsx
@@ -1,9 +1,13 @@
 import Layout from "@/components/Layout";
+import RoadMapForm from "@/components/RoadMapForm";
 import React, { useState } from "react";
 
 const CreateRoadmapPage: React.FC = () => {
-  const [formValue, setFormValue] = useState("");
-  const [title, setTitle] = useState("");
+  const [formCount, setFormCount] = useState(1);
+  const handleAddForm = () => {
+    setFormCount((formCount) => formCount + 1);
+  };
+  const handleOnSubmit = () => console.log("submit");
   return (
     <Layout>
       <main className="bg-gray-100 border-b py-8">
@@ -11,37 +15,29 @@ const CreateRoadmapPage: React.FC = () => {
           ロードマップ作成
         </h1>
         <form className="container mx-auto">
-          <div className="grid grid-cols-2 py-4 px-2 rounded-lg bg-white">
-            <div className="col-span-2 mb-4">
-              <div className="no ml-3">1</div>
-            </div>
-            <input
-              className="appearance-none bg-transparent border-b border-teal-500 w-4/5 text-gray-700 mx-auto py-1 px-2 leading-tight focus:outline-none"
-              type="text"
-              placeholder="Title"
-            />
-            <input
-              className="appearance-none bg-transparent border-b border-teal-500 w-4/5 text-gray-700 mx-auto py-1 px-2 leading-tight focus:outline-none"
-              type="url"
-              placeholder="ロードマップのURL"
-            />
-            <input
-              className="col-span-2 appearance-none bg-transparent border-b border-teal-500 w-11/12 text-gray-700 mx-auto mt-5 py-1 px-2 leading-tight focus:outline-none"
-              type="text"
-              placeholder="説明"
-            />
-          </div>
-          <div className="text-center mt-7">
-            <button className="bg-white shadow w-4/5 px-4 py-2 rounded-lg">
-              ロードマップを追加する +
-            </button>
-          </div>
-          <div className="text-right mt-7">
-            <button className="ml-8 whitespace-nowrap inline-flex items-center justify-center mr-4 px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
-              作成する
-            </button>
-          </div>
+          {[...Array(formCount)]
+            .map((_, i) => i + 1)
+            .map((id) => (
+              <RoadMapForm id={id} key={id} />
+            ))}
         </form>
+        <div className="text-center mt-7">
+          <button
+            type="button"
+            className="bg-white shadow w-4/5 px-4 py-2 rounded-lg"
+            onClick={handleAddForm}
+          >
+            ロードマップを追加する +
+          </button>
+        </div>
+        <div className="text-right mt-7">
+          <button
+            className="ml-8 whitespace-nowrap inline-flex items-center justify-center mr-4 px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+            onClick={handleOnSubmit}
+          >
+            作成する
+          </button>
+        </div>
       </main>
     </Layout>
   );
