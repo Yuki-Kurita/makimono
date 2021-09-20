486763b3636e2a9e7827e68e8c0fe47eb7e77325
Yuki-Kurita <258qu1to8yodeg1@gmail.com>
2021-09-21 00:14:02 +0900

    add header

--- src/lib/config/apolloClient.ts
+++ src/lib/config/apolloClient.ts
@@ -1,6 +1,23 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { firebase } from "@/config/firebaseConfig";
+import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+const httpLink = createHttpLink({
+  uri: `${process.env.NEXT_PUBLIC_ENDPOINT}/graphql`,
+});
+
+
+const authLink = setContext(async (_, { headers }) => {
+  const token = await firebase.auth().currentUser?.getIdToken()
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+});
 
 export const client = new ApolloClient({
-  uri: "http://localhost:3001/graphql",
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
