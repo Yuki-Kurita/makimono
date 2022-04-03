4258a8b4ae3c2304188c71c65c4df2729865903b
Yuki-Kurita <258qu1to8yodeg1@gmail.com>
2022-04-03 20:44:26 +0900

    impl roadmap header

---
+++ src/components/RoadmapDetail/RoadmapHeader.tsx
@@ -0,0 +1,65 @@
+import { Roadmap } from "@/model/types";
+import dayjs from "dayjs";
+import Image from "next/image";
+
+interface RoadmapHeaderProps {
+  roadmap: Roadmap;
+}
+
+export const RoadmapHeader: React.VFC<RoadmapHeaderProps> = ({ roadmap }) => {
+  const convertDateTime = (timestamp: string) => {
+    return dayjs(timestamp).format("YYYY/MM/DD");
+  };
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-center py-8 px-4">
+        {roadmap.title}
+      </h1>
+      <div className="flex justify-center text-lightGray">
+        <div className="mr-1">by {roadmap.author.name}</div>
+        <div className="mr-4">
+          <Image
+            src={roadmap.author.iconUrl || "/noimage.svg"}
+            className="inline object-cover w-10 h-10 rounded-full border-2 border-gray-500"
+            width={25}
+            height={25}
+            alt="user_icon"
+          />
+        </div>
+        <div>更新日 : {convertDateTime(roadmap.updatedAt)}</div>
+      </div>
+      <div className="flex justify-center">
+        <svg
+          className="w-4"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z"
+          />
+        </svg>
+        <div className="mr-2 ml-0.5">{roadmap.category.name}</div>
+        <svg
+          className="w-4 text-red-500"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+          />
+        </svg>
+        <div className="ml-0.5">{roadmap.likes}</div>
+      </div>
+    </div>
+  );
+};
--- src/pages/roadmaps/[id].tsx
+++ src/pages/roadmaps/[id].tsx
@@ -1,4 +1,5 @@
 import Layout from "@/components/common/Layout";
+import { RoadmapHeader } from "@/components/RoadmapDetail/RoadmapHeader";
 import { client } from "@/lib/config/apolloClient";
 import { FETCH_ALL_ROADMAP_IDS } from "@/lib/graphql/roadmapDetail/fetchAllRoadmapIds";
 import { FETCH_ROADMAP_BY_ID } from "@/lib/graphql/roadmapDetail/fetchRoadmapById";
@@ -59,52 +60,8 @@ export const getStaticPaths: GetStaticPaths = async () => {
 const RoadmapDetailPage: NextPage<RoadmapDetailProps> = ({ roadmap }) => {
   return (
     <Layout>
-      <main className="bg-gray-100 border-b py-8">
-        <div className="mb-8">
-          <h1 className="text-2xl font-bold ml-3 mb-4 text-center">
-            {roadmap.title}
-          </h1>
-          <div className="flex justify-center">
-            <div className="text-sm text-gray-500 mr-1">
-              by {roadmap.author.name}
-            </div>
-            <div className="text-sm text-gray-500 mr-1">
-              {roadmap.updatedAt}
-            </div>
-          </div>
-          <div className="flex justify-center">
-            <svg
-              className="w-4"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z"
-              />
-            </svg>
-            <div className="mr-2 ml-0.5">{roadmap.category.name}</div>
-            <svg
-              className="w-4 text-red-500"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-              />
-            </svg>
-            <div className="ml-0.5">{roadmap.likes}</div>
-          </div>
-        </div>
+      <main className="container mx-auto">
+        <RoadmapHeader roadmap={roadmap} />
         {/* {roadmap.items.map((it) => (
           <RoadMapItem
             title={it.title}
